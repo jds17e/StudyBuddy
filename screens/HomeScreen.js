f48722aeaa3f76adb18a3c4612e868663a09f2f1
jason <98jsantos@gmail.com>
2019-10-19 18:36:33 -0400

    Add React Native Elements

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,12 +12,16 @@ import {
 import { Card } from '../components/Card';
 import { CardSection } from '../components/CardSection';
 import { MonoText } from '../components/StyledText';
+import { Button, ThemeProvider } from 'react-native-elements';
+
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <ScrollView>
-      
+       <ThemeProvider>
+        <Button title="Hey!" />
+       </ThemeProvider>
       </ScrollView>
     </SafeAreaView>
   )
