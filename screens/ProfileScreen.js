947f64a6105c2875b186a6b4a72d32f88e11a9d7
Lucas Albano <lucas@albano.us>
2019-10-19 18:04:45 -0400

    profile styles

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,22 +1,75 @@
 import React from "react";
-import { ScrollView, StyleSheet, View, Text } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Image
+} from "react-native";
 import { ExpoLinksView } from "@expo/samples";
 
 export default function ProfileScreen() {
-        return <ScrollView style={styles.container}>
-            
-                </ScrollView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <View style={styles.title}>
+            <Image
+              style={styles.img}
+              source={require("../assets/images/splash.png")}
+            />
+            <Text style={styles.name}>Lucas Albano</Text>
+          </View>
+        </View>
+        <View style={styles.infoBox}></View>
+        <View style={styles}></View>
+        <View style={styles.content}></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 ProfileScreen.navigationOptions = {
-  title: "Profile",
+  title: "",
   header: null
 };
 
 const styles = StyleSheet.create({
+  img: {
+    width: 80,
+    height: 80,
+    borderRadius: 20
+  },
+  content: {
+    height: "100%",
+    backgroundColor: "#676767"
+  },
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+    backgroundColor: "#424242"
+  },
+  header: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "#424242",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  name: {
+    fontSize: 30,
+    padding: 20
+  },
+  infoBox: {
+    position: "absolute",
+    height: 60,
+    width: "100%",
+    borderRadius: 4,
+    marginLeft: 20,
+    marginRight: 20
   }
 });
