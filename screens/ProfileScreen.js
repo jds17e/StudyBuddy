89382c7819339667da757f81d5696746fb1f9ed8
Lucas Albano <lucas@albano.us>
2019-10-19 23:17:02 -0400

    Added more to profilepage

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Animated,
   ScrollView,
   StyleSheet,
   View,
   Text,
   SafeAreaView,
-  Image
+  Image,
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import Modal from "react-native-modal";
+import { Overlay, Card, Divider } from "react-native-elements";
 
 export default function ProfileScreen() {
+  const courses = [
+    { courseCode: "COP3014", title: "Programming I" },
+    { courseCode: "ENC1105", title: "English" },
+    { courseCode: "CEN4020", title: "Software Engineering" },
+    { courseCode: "CGS3066", title: "Web Design and Programming" }
+  ];
+  const devWidth = Dimensions.get("window").width;
+  const devHeight = Dimensions.get("window").height;
+  const [isVisible, setVisible] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
@@ -22,10 +35,43 @@ export default function ProfileScreen() {
             <Text style={styles.name}>Lucas Albano</Text>
           </View>
         </View>
-        <View style={styles.infoBox}></View>
-        <View style={styles}></View>
-        <View style={styles.content}></View>
+
+        <View style={styles.infoContainer}>
+          <View style={styles.infoBox}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.classes}>Classes</Text>
+              <Divider />
+            </View>
+
+            <View style={styles.cards}>
+              {courses.map(course => (
+                <TouchableOpacity
+                  key={course.courseCode}
+                  style={styles.cardContainer}
+                  onPress={() => setVisible(true)}
+                >
+                  <Card containerStyle={styles.card}>
+                    <Text>{course.courseCode}</Text>
+                  </Card>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+        </View>
       </ScrollView>
+      <Modal
+        isVisible={isVisible}
+        onSwipeComplete={() => setVisible(false)}
+        swipeDirection={["down"]}
+        onRequestClose={() => setVisible(false)}
+        deviceWidth={devWidth}
+        devHeight={devHeight}
+        style={styles.modal}
+      >
+        <View style={styles.modalContent}>
+          <Text>Test</Text>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -36,6 +82,24 @@ ProfileScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+  modal: {
+    margin: 0,
+    marginTop: 80,
+    justifyContent: "flex-end"
+  },
+  modalContent: {
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 15,
+    backgroundColor: "white"
+  },
+  classes: {
+    fontSize: 20
+  },
+  titleContainer: {
+    padding: 15
+  },
   img: {
     width: 80,
     height: 80,
@@ -65,11 +129,33 @@ const styles = StyleSheet.create({
     padding: 20
   },
   infoBox: {
-    position: "absolute",
-    height: 60,
-    width: "100%",
+    height: "100%",
     borderRadius: 4,
-    marginLeft: 20,
-    marginRight: 20
+    width: "100%",
+    backgroundColor: "white",
+    marginTop: 270
+  },
+  infoContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    paddingLeft: 20,
+    paddingRight: 20,
+    zIndex: 5
+  },
+  card: {
+    width: "100%",
+    height: 100,
+    margin: 0
+  },
+  cardContainer: {
+    width: "50%",
+    paddingLeft: 10,
+    paddingBottom: 10
+  },
+  cards: {
+    flex: 1,
+    flexWrap: "wrap",
+    paddingRight: 10
   }
 });
