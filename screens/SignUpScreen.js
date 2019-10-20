01a6f9f91c54b5db912d2cc906b7a11af2bcef42
Lucas Albano <lucas@albano.us>
2019-10-20 09:10:30 -0400

    sign up will send to the home screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,133 +1,154 @@
-import React, { useState }from 'react';
-import { Text, View, KeyboardAvoidingView, StyleSheet, ScrollView } from 'react-native';
-import { Input, Image, Button} from 'react-native-elements';
-import axios from 'axios'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  StyleSheet,
+  ScrollView
+} from "react-native";
+import { Input, Image, Button } from "react-native-elements";
+import axios from "axios";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { userOperations } from "../store/user";
 
-export default function SignUpScreen({navigation}) {
+const mapStateToProps = ({ user }) => ({ user });
 
+const actions = {
+  signUpUser: userOperations.signUpUser
+};
+
+const enhance = compose(
+  connect(
+    mapStateToProps,
+    actions
+  )
+);
+
+function SignUpScreen({ navigation, signUpUser }) {
   const [FirstName, setFirstName] = useState("");
   const [LastName, setLastName] = useState("");
   const [Email, setEmail] = useState("");
-	const [Username, setUsername] = useState("");
-	const [Password, setPassword] = useState("");
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+  const signUp = (Username, Password, Email, FirstName, LastName) => {
+    signUpUser(Username, Password, Email, FirstName, LastName);
+    navigation.navigate("Main");
+  };
 
   return (
-  	<KeyboardAwareScrollView
-      style={{ backgroundColor: '#424242' }}
+    <KeyboardAwareScrollView
+      style={{ backgroundColor: "#424242" }}
       resetScrollToCoords={{ x: 0, y: 0 }}
       contentContainerStyle={styles.container}
       enableAutomaticScroll={true}
       enableOnAndroid={true}
     >
-  			<View style={{height: "20%", alignItems: "center", paddingTop: "15%"}}>
-  				<Text style={{fontSize: 40, color: "white"}}> Sign Up! </Text>
-  			</View>
-	  		<Input
-	  			label='First Name'
-  				inputStyle={styles.inputStyle}
-  				containerStyle={styles.inputContainerStyle}
-          onChangeText={text => setFirstName(text)}>
-	  		</Input>
-	  		<Input
-	  			label='Last Name'
-  				inputStyle={styles.inputStyle}
-  				containerStyle={styles.inputContainerStyle}
-          onChangeText={text => setLastName(text)}>
-	  		</Input>
-	  		<Input
-	  			label='Email'
-				inputStyle={styles.inputStyle}
-				containerStyle={styles.inputContainerStyle}
-        onChangeText={text => setEmail(text)}>
-	  		</Input>
-	  		<Input
-	  			label='Username'
-				inputStyle={styles.inputStyle}
-				containerStyle={styles.inputContainerStyle}
-        onChangeText={text => setUsername(text)}>
-	  		</Input>
-	  		<Input
-	  		label='Password'
-				inputStyle={styles.inputStyle}
-				containerStyle={styles.inputContainerStyle}
-        onChangeText={text => setPassword(text)}>
-	  		</Input>
-	  		<View style={{flexDirection: "row", justifyContent: "center"}}>
-	  				<Button 
-	  				buttonStyle={{marginRight: 15, backgroundColor: "green", marginTop: "20%", width: 120, height: 50}}
-	  				title="Submit"
-	  				titleStyle={{alignSelf: "center", marginBottom: 20}}
-            onPress={() => {signUp(Username, Password, FirstName, LastName, Email)}}
-	  				/>
-	  				<Button 
-			  		buttonStyle={{marginLeft: 15, backgroundColor: "green", marginTop: "20%", width: 120, height: 50}}
-					title="Return"
-					titleStyle={{alignSelf: "center", marginBottom: 20}}
-					onPress={()=>{navigation.navigate("Welcome")}}
-			  		/>
-	  		</View>
-	  </KeyboardAwareScrollView>
+      <View style={{ height: "20%", alignItems: "center", paddingTop: "15%" }}>
+        <Text style={{ fontSize: 40, color: "white" }}> Sign Up! </Text>
+      </View>
+      <Input
+        label="First Name"
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.inputContainerStyle}
+        onChangeText={text => setFirstName(text)}
+      ></Input>
+      <Input
+        label="Last Name"
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.inputContainerStyle}
+        onChangeText={text => setLastName(text)}
+      ></Input>
+      <Input
+        label="Email"
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.inputContainerStyle}
+        onChangeText={text => setEmail(text)}
+      ></Input>
+      <Input
+        label="Username"
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.inputContainerStyle}
+        onChangeText={text => setUsername(text)}
+      ></Input>
+      <Input
+        label="Password"
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.inputContainerStyle}
+        onChangeText={text => setPassword(text)}
+      ></Input>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Button
+          buttonStyle={{
+            marginRight: 15,
+            backgroundColor: "green",
+            marginTop: "20%",
+            width: 120,
+            height: 50
+          }}
+          title="Submit"
+          titleStyle={{ alignSelf: "center", marginBottom: 20 }}
+          onPress={() => {
+            signUp(Username, Password, FirstName, LastName, Email);
+          }}
+        />
+        <Button
+          buttonStyle={{
+            marginLeft: 15,
+            backgroundColor: "green",
+            marginTop: "20%",
+            width: 120,
+            height: 50
+          }}
+          title="Return"
+          titleStyle={{ alignSelf: "center", marginBottom: 20 }}
+          onPress={() => {
+            navigation.navigate("Welcome");
+          }}
+        />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
-async function signUp(Username, Password, FirstName, LastName, Email){
-  try{
-    axios.post('http://poosproject.com/StudyBuddy/studysignup.php', {
-      "Username": Username,
-      "Password": Password,
-      "FirstName": FirstName,
-      "LastName": LastName,
-      "Email": Email,
-      "ConfirmPass": Password
-    })
-    .then(function (response) {
-    console.log(response);
-    })
-    .catch(function (error) {
-    console.log(error);
-  });
-  }catch(error){
-    console.log("Hoe")
-  }
-}
-
-
 SignUpScreen.navigationOptions = {
-  header: null,
+  header: null
 };
+
+export default enhance(SignUpScreen);
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#424242',
+    backgroundColor: "#424242",
     flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputView: {
-  	width: "100%",
-  	height: "50%",
-  	backgroundColor: "white",
+    width: "100%",
+    height: "50%",
+    backgroundColor: "white"
   },
   topView: {
-  	width: "100%",
-  	height: "20%",
-  	backgroundColor: "#bababa"
+    width: "100%",
+    height: "20%",
+    backgroundColor: "#bababa"
   },
   buttonView: {
-  	width: "100%",
-  	height: "10%",
-  	backgroundColor: "#bababa"
+    width: "100%",
+    height: "10%",
+    backgroundColor: "#bababa"
   },
   keyboard: {
-  	flex: 1
+    flex: 1
   },
   inputStyle: {
-  	color: "#bababa",
+    color: "#bababa"
   },
   inputContainerStyle: {
-  	marginBottom: "5%",
-  	width: "80%",
-  	alignSelf: "center",
+    marginBottom: "5%",
+    width: "80%",
+    alignSelf: "center"
   }
 });
