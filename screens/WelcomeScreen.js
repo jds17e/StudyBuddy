4fb0f8732bfe50a3a156bcbeff84cf8b3e2e15f6
Lucas Albano <lucas@albano.us>
2019-10-20 07:31:07 -0400

    back in business

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,114 +1,122 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, KeyboardAvoidingView, Button } from 'react-native';
-import { Overlay, Input, Image } from 'react-native-elements';
-import axios from 'axios';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Button
+} from "react-native";
+import { Overlay, Input, Image } from "react-native-elements";
+import axios from "axios";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-
-export default function WelcomeScreen({navigation}) {
-
+export default function WelcomeScreen({ navigation }) {
   const [Username, setUsername] = useState("");
   const [Password, setPassword] = useState("");
- 
+  const [user, setUser] = useState({});
+  //console.log("USER: ", JSON.parse(user));
   return (
-	  	<View style={styles.container}>
-	  		<View style={styles.logoView}>
-	  			<Image
-				  source={require("../assets/images/Logo.jpg")}
-				  style={{ width: "95%", height: 200 }}
-				/>
-	  		</View>
-	  		<View style={styles.inputView}>
-				<Input
-					label='Username'
-					inputStyle={styles.inputStyle}
-					containerStyle={styles.inputContainerStyle}
-					onChangeText={text => setUsername(text)}
-				/>
-				<Input
-					label='Password'
-					inputStyle={styles.inputStyle}
-					containerStyle={styles.inputContainerStyle}
+    <View style={styles.container}>
+      <View style={styles.logoView}>
+        <Image
+          source={require("../assets/images/Logo.jpg")}
+          style={{ width: "95%", height: 200 }}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <Input
+          label="Username"
+          inputStyle={styles.inputStyle}
+          containerStyle={styles.inputContainerStyle}
+          onChangeText={text => setUsername(text)}
+        />
+        <Input
+          label="Password"
+          inputStyle={styles.inputStyle}
+          containerStyle={styles.inputContainerStyle}
           onChangeText={text => setPassword(text)}
-				/>
-	  		</View>
-	  		<View style={styles.buttonView}>
-	  			<View style={styles.buttonStyle}>
-	  				<Button  
-	  					title="Sign In"	
-	  					onPress={() => {signIn(Username, Password)}}
-	  				/>
-	  			</View>
-	  			<View style={styles.buttonStyle}>
-	  				<Button  
-	  					title="Sign Up"
-	  					onPress={()=>{navigation.navigate("SignUp")}}
-	  				/>
-	  			</View>
-	  		</View>
-	  	</View>
-  	);
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Sign In"
+            onPress={() => {
+              signIn(Username, Password, setUser);
+            }}
+          />
+        </View>
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Sign Up"
+            onPress={() => {
+              navigation.navigate("SignUp");
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
-async function signIn(Username, Password){
-  try{
-    fetch('http://poosproject.com/StudyBuddy/studylogin.php', {
-      "Username": Username,
-      "Password": Password
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-        console.log(responseJson.FirstName);
-    })
-    .catch(function (error) {
+async function signIn(Username, Password, setUser) {
+  try {
+    axios
+      .post("http://poosproject.com/StudyBuddy/studylogin.php", {
+        Username: Username,
+        Password: Password
+      })
+      .then(response => response.data)
+      .then(res => console.log(JSON.parse(res).Email))
+      .catch(function(error) {
+        console.log(error);
+      });
+  } catch (error) {
     console.log(error);
-  });
-  }catch(error){
-    console.log(error)
   }
 }
 
 WelcomeScreen.navigationOptions = {
-  header: null,
+  header: null
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#424242",
+    backgroundColor: "#424242"
   },
   logoView: {
-  	marginTop: "10%",
-  	height: "25%",
+    marginTop: "10%",
+    height: "25%"
   },
   inputView: {
-  	marginTop: "8%",
-  	height: "20%",
+    marginTop: "8%",
+    height: "20%"
   },
   buttonView: {
-  	flex: 1,
-    alignSelf: 'stretch',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flex: 1,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginTop: "40%",
-    marginLeft: '17%',
-    marginRight: '15%',
-  }, 
+    marginLeft: "17%",
+    marginRight: "15%"
+  },
   iconStyle: {
-  	marginRight: "5%",
+    marginRight: "5%"
   },
   inputStyle: {
-  	color: "#bababa",
+    color: "#bababa"
   },
   inputContainerStyle: {
-  	marginBottom: "10%",
-  	width: "80%",
-  	alignSelf: "center",
+    marginBottom: "10%",
+    width: "80%",
+    alignSelf: "center"
   },
   keyboard: {
-  	flex: 1,
+    flex: 1
   },
   buttonStyle: {
-  	width: "30%",
+    width: "30%"
   }
 });
